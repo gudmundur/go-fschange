9dc125ca1f3954d4a0369c1e6f13622225225990
Guðmundur Bjarni Ólafsson <gudmundur.bjarni@gmail.com>
2015-03-07 17:09:51 +0100

    Add fsnotify.v1 to watch future changes

--- fschange.go
+++ fschange.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"gopkg.in/fsnotify.v1"
 )
 
 type Watcher struct {
@@ -14,6 +16,8 @@ type Watcher struct {
 
 	Events chan Event
 	Errors chan error
+
+	watcher *fsnotify.Watcher
 }
 
 type Event struct {
@@ -21,12 +25,21 @@ type Event struct {
 }
 
 func NewWatcher(since time.Time) (*Watcher, error) {
-	w := &Watcher{
-		Since:  since,
-		until:  time.Now(),
-		Events: make(chan Event),
-		Errors: make(chan error),
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
 	}
+
+	w := &Watcher{
+		Since:   since,
+		until:   time.Now(),
+		Events:  make(chan Event),
+		Errors:  make(chan error),
+		watcher: watcher,
+	}
+
+	go w.watch()
+
 	return w, nil
 }
 
@@ -36,6 +49,7 @@ func (w *Watcher) Add(path string) error {
 }
 
 func (w *Watcher) Close() error {
+	w.watcher.Close()
 	close(w.Events)
 	close(w.Errors)
 	return nil
@@ -51,6 +65,14 @@ func (w *Watcher) walkFunc(path string, info os.FileInfo, err error) error {
 		return err
 	}
 
+	if info.IsDir() {
+		err = w.watcher.Add(path)
+		if err != nil {
+			w.Errors <- err
+			return err
+		}
+	}
+
 	modTime := info.ModTime()
 	if modTime.After(w.Since) && modTime.Before(w.until) {
 		absPath, err := filepath.Abs(path)
@@ -64,17 +86,35 @@ func (w *Watcher) walkFunc(path string, info os.FileInfo, err error) error {
 	return nil
 }
 
+func (w *Watcher) watch() {
+	for {
+		select {
+		case event := <-w.watcher.Events:
+			w.Events <- Event{event.Name}
+		case err := <-w.watcher.Errors:
+			w.Errors <- err
+		}
+	}
+}
+
 func main() {
 	sinceTime := time.Now().Add(-time.Hour * 24)
-	fmt.Println(sinceTime)
 	w, _ := NewWatcher(sinceTime)
 
+	done := make(chan bool)
+
 	go func() {
-		for event := range w.Events {
-			fmt.Println(event)
+		for {
+			select {
+			case event := <-w.Events:
+				fmt.Println(event)
+			case err := <-w.Errors:
+				fmt.Println(err)
+			}
 		}
 	}()
 
 	w.Add("../")
+	<-done
 	w.Close()
 }
